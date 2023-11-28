2bacd5775f93032ff24c74d08d711006d589f843
sabirinID <108510388+sabirinID@users.noreply.github.com>
2023-11-28 09:59:48 +0700

    Update dashboard.py

--- Projects/submission/dashboard/dashboard.py
+++ Projects/submission/dashboard/dashboard.py
@@ -17,13 +17,16 @@ daily_total_rentals = main_data.groupby(pd.Grouper(key='date', freq='D'))[['non_
 daily_total_rentals['total_rentals'] = daily_total_rentals['non_registered_user_rentals'] + daily_total_rentals['registered_user_rentals']
 daily_total_rentals.reset_index(inplace=True)
 
+# Menghitung total rentals bulanan
+monthly_total_rentals = daily_total_rentals.resample('M', on='date').sum()
+
 st.title('Bike Share Dashboard')
 
 st.text('Created by Syahril Dimas Sabirin, @sabirinID')
 
 st.header('Jumlah Rental Sepeda')
 
-st.subheader('Rental Harian')
+st.subheader('Daily Rentals')
 
 col1, col2, col3 = st.columns(3)
 
@@ -49,6 +52,35 @@ ax.plot(
 )
 ax.tick_params(axis='y', labelsize=20)
 ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
+
+st.subheader('Monthly Rentals')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    min_rentals = monthly_total_rentals['total_rentals'].min()
+    st.metric('Min Rentals', value=min_rentals)
+
+with col2:
+    max_rentals = monthly_total_rentals['total_rentals'].max()
+    st.metric('Max Rentals', value=max_rentals)
+
+with col3:
+    total_rentals = monthly_total_rentals['total_rentals'].sum()
+    st.metric('Total Rentals', value=total_rentals)
+
+fig, ax = plt.subplots(figsize=(16, 9))
+ax.plot(
+    monthly_total_rentals.index,
+    monthly_total_rentals['total_rentals'],
+    marker='o', 
+    linewidth=2,
+    color='tab:blue'
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15, rotation=90)
 
 st.pyplot(fig)
 
