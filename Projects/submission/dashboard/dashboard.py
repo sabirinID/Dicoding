e2472035d5f1de9ae4d633d2322baefb8e0eb7f4
sabirinID <108510388+sabirinID@users.noreply.github.com>
2023-11-28 08:31:16 +0700

    Update dashboard.py

--- Projects/submission/dashboard/dashboard.py
+++ Projects/submission/dashboard/dashboard.py
@@ -1,21 +1,49 @@
-# Import library
+# Mengimpor pustaka
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load data
-df = pd.read_csv('main_data.csv', index=False)
+# Baca data
+main_data = pd.read_csv('main_data.csv')
 
-min_date = df['date'].min()
-max_date = df['date'].max()
- 
-with st.sidebar:
-    # Menambahkan logo
-    st.image('https://cdn.lyft.com/static/bikesharefe/logo/CapitalBikeshare-main.svg')
+# Ubah kolom 'date' menjadi datetime
+main_data['date'] = pd.to_datetime(main_data['date'])
 
-    # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label='Rentang Waktu',min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
-    )
+# Hitung total rentals harian
+daily_total_rentals = main_data.groupby(pd.Grouper(key='date', freq='D'))[['non_registered_user_rentals', 'registered_user_rentals']].sum()
+daily_total_rentals['total_rentals'] = daily_total_rentals['non_registered_user_rentals'] + daily_total_rentals['registered_user_rentals']
+daily_total_rentals.reset_index(inplace=True)
+
+st.header('Bike Share Dashboard')
+
+st.subheader('Daily Rentals')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    min_rentals = daily_total_rentals['total_rentals'].min()
+    st.metric('Min Rentals', value=min_rentals)
+
+with col2:
+    max_rentals = daily_total_rentals['total_rentals'].max()
+    st.metric('Max Rentals', value=max_rentals)
+
+with col3:
+    total_rentals = daily_total_rentals['total_rentals'].sum()
+    st.metric('Total Rentals', value=total_rentals)
+
+fig, ax = plt.subplots(figsize=(16, 9))
+ax.plot(
+    daily_total_rentals['date'],
+    daily_total_rentals['total_rentals'],
+    marker='o', 
+    linewidth=2,
+    color='tab:blue'
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
+
+st.caption('Copyright (c) Sabirin 2023')
